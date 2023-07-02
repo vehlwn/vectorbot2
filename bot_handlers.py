be80af2c3a6a4a0fb1d2ea91cabe9713552dcc71
vehlwn <36479428+vehlwn@users.noreply.github.com>
2023-07-02 08:28:24 +0400

    Use specific columns

--- bot_handlers.py
+++ bot_handlers.py
@@ -137,13 +137,13 @@ def _get_credits_string(chat_id: int, user_id: int) -> str:
     currencies: typing.Dict[str, int] = dict()
     with database.Session.begin() as session:
         result = session.execute(
-            select(User, Point, Currency)
+            select(Point.value, Currency.name)
             .join(User.points)
             .join(Point.currency)
             .where((User.chat_id == chat_id) & (User.user_id == user_id))
         )
         for row in result:
-            currencies[row.Currency.name] = row.Point.value
+            currencies[row.name] = row.value
     return "\n".join(
         [
             f"{value} {key}{strings.get_points_message_for_points(value)}"
