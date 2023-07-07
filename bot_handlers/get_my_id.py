810f1f4f875941d4e417e2f6126406f5ebc02b91
vehlwn <36479428+vehlwn@users.noreply.github.com>
2023-07-07 09:31:17 +0400

    Extend get_my_id command to support replies

--- bot_handlers/get_my_id.py
+++ bot_handlers/get_my_id.py
@@ -5,11 +5,22 @@ from async_bot import bot
 from logger import logger, create_who_triggered_str
 
 
+def _get_id_string(chat_id: int, user_id: int):
+    text = f"chat_id = {chat_id}\n"
+    text += f"user_id = {user_id}"
+    return text
+
+
 async def handle(message: telebot.types.Message):
     try:
         logger.info(f"[handle] /get_my_id: {create_who_triggered_str(message)}")
-        text = f"chat_id = {message.chat.id}\n"
-        text += f"user_id = {message.from_user.id}"
+        if message.reply_to_message is None:
+            text = "Твой id:\n"
+            text += _get_id_string(message.chat.id, message.from_user.id)
+        else:
+            r = message.reply_to_message
+            text = f"{r.from_user.first_name} id:\n"
+            text += _get_id_string(r.chat.id, r.from_user.id)
         await bot.reply_to(message, text)
     except Exception as er:
         await bot.reply_to(message, f"Error: {er}")
