3a78b3d7ca219040da3e1bbf6b921e725f61edec
vehlwn <36479428+vehlwn@users.noreply.github.com>
2023-07-07 09:04:14 +0400

    Update get_my_id command to include chat_id

--- bot_handlers/get_my_id.py
+++ bot_handlers/get_my_id.py
@@ -8,7 +8,9 @@ from logger import logger, create_who_triggered_str
 async def handle(message: telebot.types.Message):
     try:
         logger.info(f"[handle] /get_my_id: {create_who_triggered_str(message)}")
-        await bot.reply_to(message, text=str(message.from_user.id))
+        text = f"chat_id = {message.chat.id}\n"
+        text += f"user_id = {message.from_user.id}"
+        await bot.reply_to(message, text)
     except Exception as er:
         await bot.reply_to(message, f"Error: {er}")
         traceback.print_exc()
